c749bd68980e5861cd6edc8b52827dbeba8bea4d
yeonee <leenayeon@sookmyung.ac.kr>
2025-10-25 19:40:23 +0900

    init: 코드베이스 프로젝트 세팅

---
+++ 37-iOS-animation/37-iOS-animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  37-iOS-animation
+//
+//  Created by 이나연 on 10/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
