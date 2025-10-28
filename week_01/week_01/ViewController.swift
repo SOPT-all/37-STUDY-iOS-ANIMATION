2c38901c74d882583458a98d8a8751e35fcd0a1c
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-10-28 16:06:21 +0900

    setting: 1주차 과제 프로젝트 기초 세팅

---
+++ week_01/week_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week_01
+//
+//  Created by 조영서 on 10/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+        // Do any additional setup after loading the view.
+    }
+}
+
