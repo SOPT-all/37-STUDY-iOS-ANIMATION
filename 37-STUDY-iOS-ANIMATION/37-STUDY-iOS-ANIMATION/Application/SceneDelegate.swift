2b6cae9bc350ac78b8830a0cbe79ebdbe0e469dd
Yeonnies <eliyeon16@gmail.com>
2025-11-03 19:05:11 +0900

    [feat] 버튼 실습

--- 37-STUDY-iOS-ANIMATION/37-STUDY-iOS-ANIMATION/Application/SceneDelegate.swift
+++ 37-STUDY-iOS-ANIMATION/37-STUDY-iOS-ANIMATION/Application/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // 2.
         let window = UIWindow(windowScene: windowScene)
         // 3.
-        let vc = UINavigationController(rootViewController: Week1ViewController())
+        let vc = UINavigationController(rootViewController: ButtonViewController())
         // 4.
         window.rootViewController = vc
         // 5.
