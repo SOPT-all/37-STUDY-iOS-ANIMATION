56e47446c47e6446817bd2d54d9c08f1d65cba58
hyeyeonie <shy020916@naver.com>
2025-11-16 20:09:42 +0900

    [feat] Infinity Carousel 과제 구현

--- AnimationStudy/AnimationStudy/SceneDelegate.swift
+++ AnimationStudy/AnimationStudy/SceneDelegate.swift
@@ -18,10 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = UINavigationController(rootViewController: ViewController())
-        window.rootViewController = vc
-        self.window = window
+        window.rootViewController = CarouselViewController()
         window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
