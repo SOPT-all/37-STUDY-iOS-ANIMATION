a5f7b0c759737bc900542ad0a5fb0161caa7459a
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-11-30 21:36:57 +0900

    [setting] 3주차 과제 파일 세팅

---
+++ week_03/week_03/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  week_03
+//
+//  Created by 조영서 on 11/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
