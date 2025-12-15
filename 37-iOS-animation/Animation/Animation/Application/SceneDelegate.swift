bf6b6a53238a280c01586272f3f3fd129cbba1c0
jinsoeun <rosejinse@naver.com>
2025-12-15 21:35:30 +0900

    [feat] 프로그래스바 구현

--- 37-iOS-animation/Animation/Animation/Application/SceneDelegate.swift
+++ 37-iOS-animation/Animation/Animation/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = UINavigationController(rootViewController: Week02ViewController())
+        let vc = UINavigationController(rootViewController: ProgressViewController())
         window.rootViewController = vc
         self.window = window
         window.makeKeyAndVisible()
